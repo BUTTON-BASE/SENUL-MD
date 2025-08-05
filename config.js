ec3d1ed51312018d0f3e1d55d5bd397dc6d471d2
BUTTON-BASE <buttonbotbase@gmail.com>
2025-08-05 15:41:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "zf5QibQS#v_g4DwYmBkxeO3prNdFbJzjvSVCG0hw2m1ZVwVWZQPs",
+SESSION_ID: process.env.SESSION_ID || "n84XnAqY#y4sUY6Uo4rMgLhRVzzuvW5CrlNggWAjll4Bmj_Pf1SE",
 MONGODB: process.env.MONGODB || "mongodb://mongo:tZqOskNPFBvoJwXqFTbicqRdmVMzqKtt@autorack.proxy.rlwy.net:59140",
